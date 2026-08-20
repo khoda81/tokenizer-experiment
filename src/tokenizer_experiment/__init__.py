e6072b56a9cd4e9dcfd365129446dc6f8aad55a5
Mahdi Khodabandeh <42507906+khoda81@users.noreply.github.com>
2026-08-20 23:30:04 +0330

    Export byte Unigram tokenizer

--- src/tokenizer_experiment/__init__.py
+++ src/tokenizer_experiment/__init__.py
@@ -2,9 +2,11 @@ from .experiment import ExperimentConfig, run_experiment
 from .model import CausalTransformer, ModelConfig
 from .prequential import TrainConfig, run_stream_prequential
 from .tunstall import BPETokenizer, EmpiricalTunstallTokenizer
+from .unigram import ByteUnigramTokenizer
 
 __all__ = [
     "BPETokenizer",
+    "ByteUnigramTokenizer",
     "CausalTransformer",
     "EmpiricalTunstallTokenizer",
     "ExperimentConfig",
